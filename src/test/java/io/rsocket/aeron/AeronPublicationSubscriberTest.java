175d4ae16bc7c9d0f3d192df0d5a5e061e03bad8
aharonha <aharon@marketspulse.com>
2018-11-01 15:56:36 +0200

    fix test infinite loop

--- src/test/java/io/rsocket/aeron/AeronPublicationSubscriberTest.java
+++ src/test/java/io/rsocket/aeron/AeronPublicationSubscriberTest.java
@@ -5,17 +5,17 @@ import io.aeron.ConcurrentPublication;
 import io.aeron.FragmentAssembler;
 import io.aeron.Publication;
 import io.aeron.Subscription;
-import io.aeron.logbuffer.FragmentHandler;
-import io.aeron.logbuffer.Header;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
+import io.netty.buffer.ByteBufUtil;
 import io.netty.buffer.Unpooled;
 import io.rsocket.aeron.reactor.AeronPublicationSubscriber;
 import io.rsocket.aeron.reactor.AeronSubscriptionFlux;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
-import org.agrona.DirectBuffer;
+import org.junit.Assert;
 import org.junit.Test;
 import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
@@ -23,9 +23,10 @@ import reactor.core.publisher.Operators;
 import reactor.core.publisher.WorkQueueProcessor;
 
 public class AeronPublicationSubscriberTest {
+  private final int count = 10_000_000;
+
   @Test
   public void testSubscriber() throws Exception {
-    int count = 10_000_000;
     MediaDriverHolder.getInstance();
 
     Aeron aeron = Aeron.connect();
@@ -40,27 +41,36 @@ public class AeronPublicationSubscriberTest {
 
     CountDownLatch latch = new CountDownLatch(count);
     ForkJoinPool.commonPool().execute(() -> {
-      while (true) {
-        subscription.poll(new FragmentAssembler(new FragmentHandler() {
-          @Override
-          public void onFragment(DirectBuffer buffer, int offset, int length, Header header) {
-            long count1 = latch.getCount();
-            if (count1 % 10_000 == 0) {
-              System.out.println("here " + count1);
-            }
-            latch.countDown();
+      int retries = 3;
+      while (retries != 0) {
+        int poll = subscription.poll(new FragmentAssembler((buffer, offset, length, header) -> {
+          long count1 = latch.getCount();
+          if (count1 % 10_000 == 0) {
+            System.out.println("here " + count1);
           }
+          latch.countDown();
         }), 4096);
+        if (poll == 0) {
+          retries--;
+          try {
+            TimeUnit.MILLISECONDS.sleep(100);
+          } catch (InterruptedException ignoredException) {
+            break;
+          }
+        } else if (latch.getCount() > 0){
+          retries = 3;
+        } else {
+          break;
+        }
       }
     });
 
     Flux.range(1, count).map(i -> Unpooled.buffer().writeInt(i)).transform(lift).blockLast();
-    latch.await();
+    Assert.assertTrue(latch.await(5, TimeUnit.MINUTES));
   }
 
   @Test
   public void testFlux() throws Exception {
-    int count = 10_000_000;
     MediaDriverHolder.getInstance();
 
     Aeron aeron = Aeron.connect();
@@ -79,14 +89,19 @@ public class AeronPublicationSubscriberTest {
     AeronSubscriptionFlux.create("test", processor, subscription, ByteBufAllocator.DEFAULT)
         .doOnNext(b -> {
           long count1 = latch.getCount();
-          if (count1 % 100_000 == 0) {
+          if (count1 == 0) {
+            System.out.println("Done");
+          }
+          else if (count1 % 100_000 == 0) {
             System.out.println(Thread.currentThread().getName() + " - here " + count1);
           }
+          System.out.println(ByteBufUtil.prettyHexDump(b));
+
           latch.countDown();
           b.release();
         }).subscribe();
 
     Flux.range(1, count).map(i -> Unpooled.buffer().writeInt(i)).transform(lift).blockLast();
-    latch.await();
+    Assert.assertTrue(latch.await(5, TimeUnit.MINUTES));
   }
 }
